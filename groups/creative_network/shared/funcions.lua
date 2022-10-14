807e7ec6ccc0e5193e10a37cebde4b30e515eb3d
potter7k <felipems3004@gmail.com>
2022-10-14 04:05:25 -0300

    -

--- groups/creative_network/shared/funcions.lua
+++ groups/creative_network/shared/funcions.lua
@@ -1,6 +1,6 @@
 groupsService = function(permission)
     local members = {}
-    for _,v in pairs((vRP.numPermission(permission) or vRP.getUsersByPermission(permission))) do
+    for _,v in pairs((vRP.NumPermission(permission) or vRP.getUsersByPermission(permission))) do
         table.insert(members,vRP.getUserId(v))
     end
     return members
